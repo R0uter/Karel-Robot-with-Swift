fb61ff39d61a049ff94fe7b9533725ea11cdbd88
R0uter <lomyr.jo@gmail.com>
2016-02-01 17:21:06 +0800

    独立了配置文件，方便分享配置！

--- Karel Robot with Swift2/Config.swift
+++ Karel Robot with Swift2/Config.swift
@@ -1,5 +1,5 @@
 //
-//  config.swift
+//  Config.swift
 //  Karel Robot with Swift2
 //
 //  Created by R0uter on 16/2/1.
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+/**
+ *  在这里设置 Karel 的初始位置信息，比如朝向，比如坐标，比如Beeper和Block。
+ */
+struct Config {
+    typealias x = Int
+    typealias y = Int
+    typealias number = Int
+    
+    private var direction:Direction = .east
+    private var coordinate:NSPoint = NSPoint(x: 0, y: 0)
+    private var initBlock:[(x,y)] = [(3,3),(4,4)]
+    private var initBeeper:[(x,y,number)] = [(1,1,1),(2,2,8)]
+    /**
+     在这里覆盖配置，配置信息写在初始化器里，直接覆盖初始化器即可！
+     
+     direction 为Karel的朝向 分为 .east .north .south .west
+     coordinate 为Karel的初始位置
+     initBlock 为初始化墙壁信息，格式为[(x,y)]：[(3,3),(4,4)]
+     initBeeper 为初始化Beeper信息，格式为[(x,y,number)]：[(1,1,1),(2,2,8)]
+     
+     */
+    init () {
+        direction = .east
+        coordinate = NSPoint(x: 0, y: 0)
+        initBlock = [(3,3),(4,4)]
+        initBeeper = [(1,1,1),(2,2,8)]
+    }
+}//这里创建Beeper！  三个整形元组，第三个数字是Beeper的堆叠数量！前两个是坐标。
+
+
+// MARK: - 写一堆getter
+extension Config {
+    var getDirection:Direction {
+        return direction
+    }
+    var getCoordinate:NSPoint {
+        return coordinate
+    }
+    var getInitBlock:[(Int,Int)] {
+        return initBlock
+    }
+    var getInitBeeper:[(Int,Int,Int)] {
+        return initBeeper
+    }
+}
--- Karel Robot with Swift2/Run.swift
+++ Karel Robot with Swift2/Run.swift
@@ -7,23 +7,22 @@
 //
 
 class Run:Karel {
+    /**
+    在 run() 函数当中写对 Karel 的操作吧，你可以使用如下四种方法来操作 karel 机器人：
+    move()
+    turnLeft()
+    pickBeeper()
+    putBeeper()
+    另外，你还有两个功能性的函数可以使用：
+    isBeeperHere()    ←它返回一个布尔值供你判断这里是否有Beeper
+    以及 isBlocked()   ←它返回一个布尔值供你判断Karel是否被挡住了
+    不过你也可以自定义其他更多的功能，比如说下边的 turnRight() 函数。
+    
+    Hope enjoy！
+    
+     */
     func run() {
 
-/*  ————————————————————————————————————————————————————————————————————————
-        在 run() 函数当中写对 Karel 的操作吧，你可以使用如下四种方法来操作 karel 机器人：
-            move()
-            turnLeft()
-            pickBeeper()
-            putBeeper()
-        另外，你还有两个功能性的函数可以使用：
-        isBeeperHere()    ←它返回一个布尔值供你判断这里是否有Beeper
-        以及 isBlocked()   ←它返回一个布尔值供你判断Karel是否被挡住了
-       不过你也可以自定义其他更多的功能，比如说下边的 turnRight() 函数。
-        
-        Hope enjoy！
-
-—————————————————————————————————————————————————————————————————————————— */
-      
 
         move()
         
@@ -34,9 +33,7 @@ class Run:Karel {
             move()
         }
         putBeeper()
-        
-        
-        
+  
         
     }
 }
